3ea795b4d372010a1707746068e2673f5466b4e7
kashwin777 <kashwin777@gmail.com>
2026-06-01 11:36:44 +0000

    initials cicd pipeline

---
+++ DockerFile.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+## -----------------------------------
+## DevOps Utility Script
+## -----------------------------------
+#
+#echo "===== DevOps Project Helper ====="
+#
+## Show current directory
+#echo ""
+#echo "Current Directory:"
+#pwd
+#
+## Go back one directory
+#echo ""
+#echo "Moving one step back..."
+#cd ..
+#
+## Show updated directory
+#echo ""
+#echo "Updated Directory:"
+#pwd
+#
+## Create new project folder
+#echo ""
+#echo "Creating new folder..."
+#mkdir -p New_Project
+#
+## Move inside folder
+#cd New_Project
+#
+## Create files
+#echo ""
+#echo "Creating project files..."
+#
+#touch Dockerfile
+#touch README.md
+#touch app.py
+#touch .gitignore
+#
+## Add sample content
+#echo "# DevOps Project" > README.md
+#
+#echo "print('Hello DevOps')" > app.py
+#
+#echo "FROM ubuntu:latest" > Dockerfile
+#
+## Show created files
+#echo ""
+#echo "Project Files:"
+#ls -la
+#
+#echo ""
+#echo "Setup Completed Successfully!"
+
